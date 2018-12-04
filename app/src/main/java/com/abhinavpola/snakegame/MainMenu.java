493d02b5dae68f74b4e729e2390947c88226b767
polaa <pola2@illinois.edu>
2018-12-04 14:28:00 -0600

    completed menu button flow

--- app/src/main/java/com/abhinavpola/snakegame/MainMenu.java
+++ app/src/main/java/com/abhinavpola/snakegame/MainMenu.java
@@ -1,7 +1,9 @@
 package com.abhinavpola.snakegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void playClick(View view) {
+        Intent i=new Intent(MainMenu.this,
+                Player.class);
+        startActivity(i);
+    }
+
+    public void leaderClick(View view) {
+        Intent i = new Intent(MainMenu.this,
+                Leaderboard.class);
+        startActivity(i);
+    }
+
+    public void settingsClick(View view) {
+        Intent i = new Intent(MainMenu.this,
+                Settings.class);
+        startActivity(i);
     }
 }
