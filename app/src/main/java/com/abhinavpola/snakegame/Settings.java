dd4caa37fb3c3398b3d60b950b50b375d1cd8330
polaa <pola2@illinois.edu>
2018-12-04 14:39:07 -0600

    updated ui design

--- app/src/main/java/com/abhinavpola/snakegame/Settings.java
+++ app/src/main/java/com/abhinavpola/snakegame/Settings.java
@@ -1,13 +1,23 @@
 package com.abhinavpola.snakegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class Settings extends AppCompatActivity {
+
+    GoogleSignInAccount account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void signOut(View view) {
+
     }
 }
